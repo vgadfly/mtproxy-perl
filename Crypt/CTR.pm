860bdb59e1181bf485c182a622ee6b7727b822d3
Vasiliy Vylegzhanin <6vasia@gmail.com>
2019-10-29 13:25:26 +0300

    fix: CTR in proper stream mode

--- Crypt/CTR.pm
+++ Crypt/CTR.pm
@@ -13,6 +13,7 @@ sub bn2bin
     my $blen = $bn->num_bytes;
     my $bin = $bn->to_bin;
     $bin = "\x0"x($len-$blen) . $bin if $blen < $len;
+    $bin = substr($bin, 0, $len) if $blen > $len;
     return $bin;
 }
 
@@ -43,14 +44,26 @@ sub encrypt
     my $bn_ctx = Crypt::OpenSSL::Bignum::CTX->new;
     my $ct = Crypt::OpenSSL::Bignum->zero;
 
-    my $pad = Crypt::OpenSSL::Random::random_pseudo_bytes( 
-        -(length($data)) % $block_size);
-
-    $data .= $pad;
-
-    my $block_count = length($data) / $block_size;
-
     my $enc_data;
+
+    if ( defined $self->{_stash} ) {
+        my $slen = length( $self->{_stash} );
+
+        if ( $slen < length($data) ) {
+            my $slice = substr( $data, 0, $slen );
+            $data = substr( $data, $slen );
+            $enc_data .= $slice ^ $self->{_stash};
+            $self->{_stash} = undef;
+        }
+        else {
+            my $slice = substr( $self->{_stash}, 0, length($data) );
+            $enc_data .= $data ^ $slice;
+            $self->{_stash} = substr( $self->{_stash}, length($data) );
+            $data = '';
+        }
+    }
+
+    my $block_count = int( length($data) / $block_size );
     for( my $b=0; $b < $block_count; $b++ ) 
     {
         my $iv = $self->{iv};
@@ -62,6 +75,22 @@ sub encrypt
 
         $enc_data .= substr($data, $block_size * $b, $block_size) ^ $gamma;
     }
+
+    if ( $block_count * $block_size < length($data) ) {
+        $data = substr( $data, $block_count * $block_size );
+        
+        my $iv = $self->{iv};
+        my $gamma = $self->{cipher}->encrypt( $iv );
+        my $slice = substr( $gamma, 0, length($data) );
+        $self->{_stash} = substr( $gamma, length($data) );
+        
+        $ct = Crypt::OpenSSL::Bignum->new_from_bin( $iv );
+        $ct = $ct->add(Crypt::OpenSSL::Bignum->one);
+        $self->{iv} = bn2bin($ct, 16);
+        
+        $enc_data .= $data ^ $slice;
+    }
+
     return $enc_data;
 }
 
